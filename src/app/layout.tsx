4041131614c33cddcd3caa317bcab5fb412a0ee8
Waleed Salah <walidsalah652@gmail.com>
2025-08-25 16:37:32 +0300

    feat: add meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,42 @@ export const metadata: Metadata = {
     template: "%s | Native Ui",
   },
   description:
-    "Native Ui is a small custom ui components and hooks documentation",
+    "Native UI is a collection of reusable React components and hooks built with React, TypeScript, and Tailwind CSS. No extra UI libraries, just pure React power.",
+  keywords: [
+    "React",
+    "UI",
+    "Components",
+    "Hooks",
+    "Reusable",
+    "Tailwind",
+    "TypeScript",
+    "Vite",
+    "Next.js",
+    "Lightweight UI",
+  ],
+  authors: [{ name: "Waleed Salah" }],
+  openGraph: {
+    type: "website",
+    url: "https://native-ui.vercel.app/",
+    title: "Native UI – React Components & Hooks",
+    description:
+      "Build faster with lightweight, customizable React components and hooks. No extra libraries needed – just React, TypeScript, and Tailwind CSS.",
+    images: [
+      {
+        url: "https://native-ui.vercel.app/native-ui.png",
+        // width: 1200,
+        // height: 630,
+        alt: "Native UI – React Components & Hooks",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Native UI – React Components & Hooks",
+    description:
+      "Lightweight, customizable React components and hooks. Pure React with Tailwind CSS – no extra UI libraries required.",
+    images: ["https://native-ui.vercel.app/native-ui.png"],
+  },
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
